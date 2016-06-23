d0f4493bb7a432e5122b5bb3074b59ce438a4fcc
bettybach1 <dsd12@SD-12.fasfg.local>
2016-06-23 10:55:37 +0100

    Changed DoctorServlet

--- src/main/java/controller/DoctorServlet.java
+++ src/main/java/controller/DoctorServlet.java
@@ -1,20 +1,20 @@
 package controller;
 
 import java.io.IOException;
-
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+
 import model.Doctor;
 
-/**
- * Servlet implementation class DoctorServlet
- */
+@WebServlet({ "/DoctorServlet", "/doctor" })
+@SuppressWarnings("unchecked")
 public class DoctorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
