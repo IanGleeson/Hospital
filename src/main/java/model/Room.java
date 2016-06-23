7ef3d04782e86b60fd20e4f3807cb84cc91510c9
Gints Gavars <gintsg@gmail.com>
2016-06-23 11:09:24 +0100

    Added getters & setters

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -26,6 +26,83 @@ public class Room {
 	@JoinColumn(name="RoomType")
 	private int Type;
 	
+	@Column
+	@OneToMany(cascade =CascadeType.ALL)
+	@JoinColumn(name="Name")
+	private int Name;
+	
 	List<Bed> listOfBeds;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return Id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		Id = id;
+	}
+
+	/**
+	 * @return the wardId
+	 */
+	public int getWardId() {
+		return WardId;
+	}
+
+	/**
+	 * @param wardId the wardId to set
+	 */
+	public void setWardId(int wardId) {
+		WardId = wardId;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public int getType() {
+		return Type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		Type = type;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public int getName() {
+		return Name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(int name) {
+		Name = name;
+	}
+
+	/**
+	 * @return the listOfBeds
+	 */
+	public List<Bed> getListOfBeds() {
+		return listOfBeds;
+	}
+
+	/**
+	 * @param listOfBeds the listOfBeds to set
+	 */
+	public void setListOfBeds(List<Bed> listOfBeds) {
+		this.listOfBeds = listOfBeds;
+	}
+	
+	
 	
 }
